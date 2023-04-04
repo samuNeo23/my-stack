501c09e6d9b61b765c9b41a1818f7469915cea20
samuNeo23 <127971869+samuNeo23@users.noreply.github.com>
2023-04-04 13:03:32 +0200

    Update MyStackTest.java

--- MyStackTest.java
+++ MyStackTest.java
@@ -1,29 +1,35 @@
-package mystack;
+package teststack;
 
-public class MyStackTest {
+/**
+ *
+ * @author Samuele
+ */
+public class TestStack {
 
     public static void main(String[] args) {
+
         MyStack stack = new MyStack(5);
+        stack.push(10);
+        stack.push(10);
         stack.push(4);
         stack.push(4);
-        stack.push(4);
-        stack.push(4);
-        stack.push(4);
-        stack.push(4);
-        stack.push(4);
+        stack.push(10);
+
         int elem;
-        try{
-            elem = stack.pop();
-            elem = stack.pop();
-            elem = stack.pop();
-            elem = stack.pop();
-            elem = stack.pop();
-            elem = stack.pop();
-            elem = stack.pop();
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-        }
-     
-    }
-    
+//        try{
+//            elem = stack.pop();
+//            elem = stack.pop();
+//            elem = stack.pop();
+//            elem = stack.pop();
+//            elem = stack.pop();
+//            elem = stack.pop();
+//            elem = stack.pop();
+//        }catch(Exception e){
+//            System.out.println(e.getMessage());
+//        }
+        System.out.println(stack.top());
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.toString());
+    }//fine main
+
 }
